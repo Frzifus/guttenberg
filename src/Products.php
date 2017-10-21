e74b4032e4692e7e584da8a19f1bb50225e25829
frzifus <benne@klimlive.de>
2017-10-22 00:02:41 +0200

    make that stuff php valid

--- src/Products.php
+++ src/Products.php
@@ -11,28 +11,28 @@
 
 class Products {
   /** @Id @Column(type="integer") @GeneratedValue */
-  private int id;
+  private $id;
   // webseite von wo geklaut wurde
   /** @prefix @Column(type="integer")  */
-  private string prefix;
+  private $prefix;
   // productname
   /** @products_name @Column(type="varchar255") */
-  private string products_name;
+  private $products_name;
   // Artikelnummer
   /** @products_model @Column(type="varchar64")  */
-  private string products_model;
+  private $products_model;
   /** @products_ean @Column(type="varchar128") */
-  private string products_ean;
+  private $products_ean;
   /** @products_image @Column(type="varchar254") */
-  private string products_image;
+  private string $products_image;
   /** @products_description @Column(type="text") */
-  private string products_description;
+  private $products_description;
   /** @products_short_description @Column(type="text") */
-  private string products_short_description;
+  private $products_short_description;
   /** @products_price @Column(type="decimal(15.4)") */
-  private double products_price;
+  private $products_price;
   /** @products_weight @Column(type="decimal(15.4)") */
-  private double products_weight;
+  private $products_weight;
 
   // /** @products_keywords @Column(type="varchar255") */
   // private string products_keywords;
@@ -44,34 +44,34 @@ class Products {
    * public methods
    */
 
-  public void __construct() {}
+  public function __construct() {}
 
-  public void set_Prefix(string prefix) {
-    $this->prefix = prefix;
+  public function set_Prefix(string $prefix) {
+    $this->prefix = $prefix;
   }
 
-  public void set_Name(string name) {
-    $this->products_name = name;
+  public function set_Name(string $name) {
+    $this->products_name = $name;
   }
 
-  public void set_Model(string model) {
-    $this->products_model = model;
+  public function set_Model(string model) {
+    $this->products_model = $model;
   }
 
-  public void set_EAN(string ean) {
-    $this->products_ean = ean;
+  public function set_EAN(string $ean) {
+    $this->products_ean = $ean;
   }
 
-  public void set_ImagePath(string path) {
-    $this->products_image = path;
+  public function set_ImagePath(string $path) {
+    $this->products_image = $path;
   }
 
-  public void set_Description(string desc) {
-    $this->products_description = desc;
+  public function set_Description(string $desc) {
+    $this->products_description = $desc;
   }
 
-  public void set_ShortDescription(string desc) {
-    $this->products_short_description = desc;
+  public function set_ShortDescription(string $desc) {
+    $this->products_short_description = $desc;
   }
   /**
    * private methods
