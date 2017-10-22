5129799765c4f4f154d3165e63d276d049db4d84
frzifus <benne@klimlive.de>
2017-10-22 18:16:51 +0200

    Added tuple what includes pattern and content

---
+++ src/ContentTuple.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Description of ContentTuple
+ *
+ * @author Frzifus
+ *
+ */
+
+class ContentTuple {
+  public $Pattern;
+  public $Content;
+  public function __construct() {
+    $this->Pattern = null;
+    $this->Content = null;
+  }
+}
+
+?>
