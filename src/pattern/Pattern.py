4edf4e45a7cb887bddcf0de899ef591ab51b3507
frzifus <benne@klimlive.de>
2017-10-22 02:25:12 +0200

    add a small python script to make gen pattern

---
+++ src/pattern/Pattern.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+
+# Make it work for Python 2+3 and with Unicode
+import io
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
+
+print("[Build DOM pattern]\n")
+prefix = input("Prefix: ")
+# Define data
+data = {
+    "prefix": prefix,
+    "DOMPath": {
+        "products_name": input("Name: "),
+        "products_model": input("Model: "),
+        "products_ean": input("EAN: "),
+        "products_description": input("Description: "),
+        "products_short_description": input("Short description: "),
+        "products_price": input("Price: "),
+        "products_weight": input("Weight: "),
+        "products_image": []
+
+    }
+}
+
+print("[ImagePath \"e\" to continue!]\n")
+index = 0
+while (True):
+    path = input("ImagePath: ")
+    if (path == "e"):
+        break
+    data["DOMPath"]["products_image"].append(path)
+    index += 1
+print("Write " + prefix + ".json\n")
+# Write JSON file
+with io.open(prefix + '.json', 'w', encoding='utf8') as outfile:
+    str_ = json.dumps(data, indent=2, sort_keys=True, separators=(',', ': '),
+                      ensure_ascii=False)
+    outfile.write(to_unicode(str_))
